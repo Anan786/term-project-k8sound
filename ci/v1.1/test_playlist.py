26c34058ea2672c0cfdeff14fc9651c987498813
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-03-07 00:21:05 -0800

    Update Dockerfile and fix playlist test

--- ci/v1.1/test_playlist.py
+++ ci/v1.1/test_playlist.py
@@ -19,13 +19,13 @@ def test_simple_run(pserv, playlist_ex):
     first_playlist = [playlist_ex[0], playlist_ex[1]]
     trc, p_id = pserv.create(first_playlist)
     assert trc == 200
-    trc, p_list = pserv.read(p_id)
+    trc, p_list = pserv.get(p_id)
     assert trc == 200 and p_list == first_playlist
 
     # Test updating
     trc = pserv.update(p_id, [playlist_ex[2]])
     assert trc == 200
-    trc, p_list = pserv.read(p_id)
+    trc, p_list = pserv.get(p_id)
     assert trc == 200 and p_list == playlist_ex
 
     trc = pserv.delete(p_id)

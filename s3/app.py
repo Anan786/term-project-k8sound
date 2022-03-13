7a0498bcb966320056d90ba1626991d36a079a82
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-03-12 16:42:44 -0800

    Add update playlist method

--- s3/app.py
+++ s3/app.py
@@ -5,20 +5,17 @@ Playlist Service
 
 # Standard library modules
 import logging
-import sys  # noqa: F401
-import time  # noqa: F401
+import sys
 
 # Installed packages
 from flask import Blueprint
 from flask import Flask
-from flask import request  # noqa: F401
+from flask import request
 from flask import Response
-
-import jwt  # noqa: F401
 
 from prometheus_flask_exporter import PrometheusMetrics
 
-import requests  # noqa: F401
+import requests
 
 import simplejson as json
 
@@ -64,7 +61,28 @@ def readiness():
 
 @bp.route('/<playlist_id>', methods=['PUT'])
 def update_playlist(playlist_id):
-    return Response("", status=200, mimetype="application/json")
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        # TODO: set the owner based on the login certificate
+        # MusicIds: list of id => [1, 2, 3]
+        OwnerId = content['OwnerId']
+        MusicIds = content['MusicIds']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    payload = {"objtype": "playlist", "objkey": playlist_id}
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.put(
+        url,
+        params=payload,
+        json={"objtype": "playlist", "OwnerId": OwnerId, "MusicIds": MusicIds},
+        headers={'Authorization': headers['Authorization']})
+    return (response.json())
 
 
 @bp.route('/', methods=['POST'])
